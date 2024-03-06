689c89a8c248defea208b465e5f0b734181c7ea3
piptarou <piptarou@gmail.com>
2024-03-06 19:12:33 -0400

    finished new user script

--- userAdd.sh
+++ userAdd.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ $(cat /etc/passwd | grep eli | wc -l) -gt 0 ]; then
-	echo "User Eli exists"
-else
-      useradd -m eli
-      echo "User Eli created"
-
-fi
+while read user ;
+do
+	if [[ $(cat /etc/passwd | grep $user | wc -l) > 0 ]]; then
+		echo "User $user exists"
+	else
+		useradd -m $user
+		echo "User $user created"
+	fi
+done <userList.csv
